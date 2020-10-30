170fde2ec1fd0efdca17f695dd69a4fb093f5c87
Martins <martins.jansevskis@gmail.com>
2020-10-30 16:55:29 +0200

    Initial

---
+++ app/src/main/java/lv/martins/vrvthello/MainActivity.java
@@ -0,0 +1,30 @@
+package lv.martins.vrvthello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button gotonext = findViewById(R.id.button);
+        gotonext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FullscreenActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
