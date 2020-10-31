8f3e877619c57ec7d6250f81f0bd2b9d6a7bc555
Martins <martins.jansevskis@gmail.com>
2020-10-31 09:58:18 +0200

    design

--- app/src/main/java/lv/martins/vrvthello/MainActivity.java
+++ app/src/main/java/lv/martins/vrvthello/MainActivity.java
@@ -3,9 +3,13 @@ package lv.martins.vrvthello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.e("VRVT", "onCreate method");
+
         Button gotonext = findViewById(R.id.button);
         gotonext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, FullscreenActivity.class));
+//                startActivity(new Intent(MainActivity.this, FullscreenActivity.class));
+                Snackbar.make(v, "This is snackbar", Snackbar.LENGTH_SHORT)
+                        .show();
             }
         });
     }
@@ -26,5 +34,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+
+        Log.e("VRVT", "onPause method");
     }
 }
